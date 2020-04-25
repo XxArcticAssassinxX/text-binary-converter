98599ca0065f232b3b4d4da4c84384c5fe19e6c2
αrcтιѕ <34276587+XxArcticAssassinxX@users.noreply.github.com>
2020-04-24 22:38:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 import re
+import time
 
 
 def converttobinary():
     x = input("\033[0;37;48mEncrypt: ")
+    time.perf_counter_ns()
     y = ' '.join(format(ord(i), 'b') for i in x)
+    binaryTime = time.perf_counter_ns()
     print("\033[0;37;48mBinary:", y)
+    print("Converted to binary in", binaryTime, "nanoseconds.")
     options()
 
 
@@ -13,13 +17,16 @@ def converttotext():
     a = input("\033[0;37;48mDecrypt: ")
     pattern = re.compile('^[01 ]*$')
     if pattern.fullmatch(a):
+        time.perf_counter_ns()
         a = a.split(" ")
         b = ""
         for binary_value in a:
             c = int(binary_value, 2)
             d = chr(c)
             b += d
+        textTime = time.perf_counter_ns()
         print(b)
+        print("Binary converted to text in", textTime, "nanoseconds.")
         options()
     else:
         print("\033[0;31;50mMake sure your input is in binary.")
