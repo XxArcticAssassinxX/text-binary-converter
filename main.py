3f1344a7f528cef56533921263c852b13661a768
Reinny <reynsth@gmail.com>
2020-05-05 09:13:04 -0400

    small refractor

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import sys
 import re
 import time
 
+gray_text = "\033[0;37;48m"
+red_text = "\033[0;31;50m"
 
 def converttobinary():
-    x = input("\033[0;37;48mEncrypt: ")
+    x = input("{}Encrypt: ").format(gray_text)
     time.perf_counter_ns()
     y = ' '.join(format(ord(i), 'b') for i in x)
     binaryTime = time.perf_counter_ns()
-    print("\033[0;37;48mBinary:", y)
+    print("Binary:", y).format(gray_text)
     print("Converted to binary in", binaryTime, "nanoseconds.")
     options()
 
 
 def converttotext():
-    a = input("\033[0;37;48mDecrypt: ")
+    a = input("{}Decrypt: ").format(gray_text)
     pattern = re.compile('^[01 ]*$')
     if pattern.fullmatch(a):
         time.perf_counter_ns()
@@ -29,26 +31,20 @@ def converttotext():
         print("Binary converted to text in", textTime, "nanoseconds.")
         options()
     else:
-        print("\033[0;31;50mMake sure your input is in binary.")
+        print("{}Make sure your input is in binary.").format(red_text)
         options()
 
 
 def options():
-    e = input("\033[0;37;48mEncrypt or decrypt(e/d): ")
+    e = input("{}Encrypt or decrypt(e/d): ").format(gray_text).lower()
     if e == "e":
-        converttobinary()
-    elif e == "E":
         converttobinary()
     elif e == "d":
         converttotext()
-    elif e == "D":
-        converttotext()
     elif e == "quit":
         sys.exit()
-    elif e == "Quit":
-        sys.exit()
     else:
-        print("\033[0;31;50mEnter 'e', 'd', or 'quit'.")
+        print("Enter 'e', 'd', or 'quit'.").format(red_text)
         options()
 
 
